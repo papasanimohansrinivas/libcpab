758709655e4e8b484e30383fc9ae207a70fc468b
papasani\mohan/srinivas <papasani.mohansrinivas@gmail.com>
2021-07-20 13:55:13 +0530

    measure time for each line of code

--- examples/simple_demo.py
+++ examples/simple_demo.py
@@ -20,6 +20,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 import cv2
+import time 
+
 
 #%%
 def argparser():
@@ -44,31 +46,61 @@ if __name__ == "__main__":
     
     # Number of transformed samples 
     N = 9
+    time1  = time.time()
     
     # Load some data
     data = plt.imread(get_dir(__file__) + '/../data/cat.jpg') 
     data = np.tile(data[None], [N,1,1,1]) # create batch of data
-    
+    time2 = time.time()
+    print("--- loading file time ",time2-time1)
     # Create transformer class
+    time3 = time.time()
     T = Cpab([3, 3], backend=args.backend, device=args.device, 
              zero_boundary=True, volume_perservation=False, override=False)
+    time4 = time.time()
+    print("--- loading Cpab class ",time4-time3)
 
     # Sample random transformation
-    theta = T.sample_transformation(N)
-    
+    time5  = time.time()
+    theta  = T.sample_transformation(N)
+    time6  = time.time()
+    print("--- getting sample transformation ",time6-time5)
+
     # Convert data to the backend format
-    data = T.backend.to(data, device=args.device)
-    
+    time7 = time.time()
+    data  = T.backend.to(data, device=args.device)
+    time8 = time.time()
+    print("---- transfer data from cpu to gpu ",time8-time7)
+
     # Pytorch have other data format than tensorflow and numpy, color 
     # information is the second dim. We need to correct this before and after
+    time9 = time.time()
     data = data.permute(0,3,1,2) if args.backend=='pytorch' else data
+    time10 = time.time()
 
+    print("---- change shape of data ",time10-time9)
     # Transform the images
+    time11 = time.time()
+
     t_data = T.transform_data(data, theta, outsize=(350, 350))
+
+    time12  = time.time()
     
+    print("--- final transformation",time12-time11)
+
+    time13 = time.time()
+
+
     # Get the corresponding numpy arrays in correct format
     t_data = t_data.permute(0,2,3,1) if args.backend=='pytorch' else t_data
+    time14 = time.time()
+
+    print("--- changing back shape from pytorch",time14-time13)
+
+    time15  = time.time()
     t_data = T.backend.tonumpy(t_data)
+    time16 = time.time()
+    print("--- back to numpy format",time16-time15)
     print(len(t_data))
     counter_ = 1
     for data_ in t_data:
